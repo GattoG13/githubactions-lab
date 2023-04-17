19be41dc90844e669425a62de13924020bf9f24f
Ellargo <federico@barcelo.com.uy>
2023-04-17 16:20:54 -0300

    ASD

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -8,6 +8,3 @@ aws s3 sync ./_next/data s3://$S3_ORIGIN_BUCKET/_next/data --metadata-directive 
 
 # Sync HTML and other files with no cache
 aws s3 sync ./ s3://$S3_ORIGIN_BUCKET --exclude "common/favicons/*" --exclude "_next/*" --exclude "common/assets/*" --include "common/favicons/site.webmanifest" --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --delete
-
-# Invalidate cache
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
